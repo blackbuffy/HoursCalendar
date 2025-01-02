2ea02638a34af118c46845dfdcfb1e8dcaea9723
buffy <132291503+blackbuffy@users.noreply.github.com>
2025-01-02 18:35:24 +0300

    Revert "Редактил TopAppBar"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.wellon.hourscalendar"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.wellon.hourscalendar"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
