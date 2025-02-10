a8c5ea057d705f29c6e785db8c2efa7e0833f192
buffy <kengallivanu@gmail.com>
2025-02-10 23:21:35 +0300

    +анимация цвета числа +изменение размера и его анимация при выборе

--- app/src/main/java/com/wellon/hourscalendar/composables/calendar/Day.kt
+++ app/src/main/java/com/wellon/hourscalendar/composables/calendar/Day.kt
@@ -1,5 +1,7 @@
 package com.wellon.hourscalendar.composables.calendar
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -11,6 +13,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -36,6 +39,10 @@ fun Day(
     var hours = 0
     var isWrittenHours = false
 
+    val animatedDayColor by animateColorAsState(
+        targetValue = if (isSelected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
+    )
+
     dates.keys.forEach {
         println(it)
     }
@@ -51,7 +58,7 @@ fun Day(
 
     Card(
         modifier = Modifier
-            .padding(4.dp)
+            .padding(if (isSelected) 2.dp else 4.dp)
             .aspectRatio(2f)
             .graphicsLayer(
                 shadowElevation = 4f,
@@ -61,21 +68,21 @@ fun Day(
             ),
         shape = RoundedCornerShape(8.dp)
     ) {
-        val gradientBrush = Brush.linearGradient(
-            0.3f to MaterialTheme.colorScheme.secondaryContainer,
-            0.6f to Color(0xFFB0E0E6),
-            0.8f to Color(0xFF87CEEB),
-            1f to Color(0xFF4682B4)
-        )
+        val gradientBrushStart = getGradient(0)
+        val gradientBrushEnd = getGradient(hours)
+
+
 
         val modifier = if (isWrittenHours) Modifier
+            .animateContentSize()
             .fillMaxSize()
             .clickable {
                 onClick(dayState)
                 selectionState.onDateSelected(date)
             }
-            .background(brush = gradientBrush)
+            .background(brush = gradientBrushEnd)
         else Modifier
+            .animateContentSize()
             .fillMaxSize()
             .clickable {
                 onClick(dayState)
@@ -90,8 +97,42 @@ fun Day(
             Text(
                 text = state.date.dayOfMonth.toString(),
                 style = if (isSelected) MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold) else MaterialTheme.typography.titleSmall,
-                color = if (isSelected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
+                color = animatedDayColor
             )
         }
+    }
+}
+
+@Composable
+fun getGradient(hours: Int): Brush {
+    return when (hours) {
+        1 -> Brush.linearGradient(
+            0.7f to MaterialTheme.colorScheme.secondaryContainer,
+            1f to Color(0xFFB0E0E6)
+        )
+        2 -> Brush.linearGradient(
+            0.6f to MaterialTheme.colorScheme.secondaryContainer,
+            1f to Color(0xFFB0E0E6)
+        )
+        3 -> Brush.linearGradient(
+            0.5f to MaterialTheme.colorScheme.secondaryContainer,
+            1f to Color(0xFFB0E0E6)
+        )
+        4 -> Brush.linearGradient(
+            0.4f to MaterialTheme.colorScheme.secondaryContainer,
+            1f to Color(0xFFB0E0E6)
+        )
+        5 -> Brush.linearGradient(
+            0.3f to MaterialTheme.colorScheme.secondaryContainer,
+            1f to Color(0xFFB0E0E6)
+        )
+        6 -> Brush.linearGradient(
+            0.2f to MaterialTheme.colorScheme.secondaryContainer,
+            1f to Color(0xFFB0E0E6)
+        )
+        else -> Brush.linearGradient(
+            0.3f to MaterialTheme.colorScheme.secondaryContainer,
+            1f to MaterialTheme.colorScheme.secondaryContainer
+        )
     }
 }
